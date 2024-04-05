0ff434aa6d8264a891b3587b222662096a3ae649
Harsh10 <142471397+Harshchaudhary10@users.noreply.github.com>
2024-04-05 09:40:45 +0530

    Add files via upload

---
+++ Day 3/js-intro/script.js
@@ -0,0 +1,35 @@
+// function env(){
+// // console.log(message);
+// // var message='hello';
+// // console.log(message);
+// // message="else";
+// // console.log(message);
+// for(var i=0;i<5;i++){
+//     console.log(i);
+// }
+// console.log(i)
+// }
+// env();
+// let arr=['a','b','c','d']
+// let cpy=[...arr]
+// let a='12.12'
+// let b=20
+// let d= b+a*1
+
+// console.log(cpy)
+// const str='khan';
+// const usr='my name is ';
+// console.log(usr)
+// function sum(a,b){
+//     console.log(a+b)
+// }
+// sum(10)
+
+const obj={
+    1: 'one',
+    'name':'harsh',
+
+    12:'twelve'
+}
+const x=prompt('hbdfbbn')
+console.log(obj[x])
